69e42beb814e645774218fd2ff37a8df99b7c82a
malfaroe <mauricio.alfaro.espinoza@gmail.com>
2022-10-05 17:01:01 -0300

    New preparatory units for preprocessing

--- random_noise.py
+++ random_noise.py
@@ -6,38 +6,39 @@ import soundfile as sf
 import io
 from scipy.io.wavfile import write
 
-
-
 """
-1. Lee los archivos del directorio
-2. Informa sus nombres
-3. load a nympy array de cada uno usando librosa
-4. Reporta su sr_original y duración"""
+1. reads the file
+2. Spec file
+2. Generates the random file the same size of the file
+4. Back generated to wav
+5. Saves the random in the random noise dir"""
 
 files_dir = r"C:\Users\malfaro\Desktop\mae_code\Denoiser\data_toy"
 generated_dir = r"C:\Users\malfaro\Desktop\mae_code\Denoiser\generated"
+random_dir = r"C:\Users\malfaro\Desktop\mae_code\Denoiser\randomNoise"
+
 FRAME_SIZE = 2048
 HOP_SIZE = 512
 #1. Lee archivos
 files_names= os.listdir(files_dir)
-
-#2. Nombres
-print ("Files in the folder:")
-print("______________________")
-for file in files_names:
-    print(file)
 
 #3. Load a to a numpy file each file
 spec_files = [] #lista para guardar todos losa arrays en una matriz
 for i, file in enumerate(files_names):
     numpy_file, sr_original = librosa.load(os.path.join(files_dir, file))
     spec_file = librosa.stft(numpy_file,  n_fft = FRAME_SIZE, hop_length = HOP_SIZE)
-    print("Spec file size (nr_freq_bins, nr_time_bins):", spec_file.shape )
+    print("File read size (nr_freq_bins, nr_time_bins):", spec_file.shape )
+    #generate the random noise
+    noise_spec = np.random.randn(spec_file.shape[0], spec_file.shape[1])
     #Back to wav
     S = np.abs(spec_file)**2
     y_inv = librosa.griffinlim(S)
-    save_path = os.path.join(generated_dir, str(i) + ".wav")
-    sf.write(save_path, y_inv, sr_original)
+    noise = librosa.griffinlim(noise_spec)
+    #Mixing the file
+    noised_file = y_inv + noise
+    #Save
+    save_path = os.path.join(random_dir, str(i) + ".wav")
+    sf.write(save_path, noised_file, sr_original)
     
 
 
